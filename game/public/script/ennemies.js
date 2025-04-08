b108f76ab89f20bfd2e62b21cac4da37fa40193e
Yann <yann.lestage@gmail.com>
2025-04-08 11:37:03 +0200

    git init

---
+++ game/public/script/ennemies.js
@@ -0,0 +1,42 @@
+export class NormalRat{
+    constructor(hp, type, name,id_tile, image, money) {
+        this.hp = hp;
+        this.type = type;
+        this.name = name;
+        this.id_tile = id_tile;
+        this.image = image;
+        this.money = money;
+    }
+
+    setHp(hp) {
+        this.hp = hp
+    }
+
+    getHp() {
+        return this.getHp;
+    }
+
+    getType() {
+        return this.type;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setIdTile(id_tile) {
+        this.id_tile = id_tile;
+    }
+
+    getIdTile() {
+        return this.id_tile;
+    }
+
+    getImage() {
+        return this.image;
+    }
+
+    getMoney() {
+        return this.money
+    }
+}
